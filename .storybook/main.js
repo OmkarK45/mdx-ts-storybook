c04c3ae36d90c3b0074f8085e3028d4355cbf817
Omkar <omkarak45@gmail.com>
2022-07-21 14:10:06 +0530

    chore: removed unused packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,8 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5"
-  },
-  webpackFinal: async (config, { configType }) => {
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        fs: false,
-        path: false,
-        os: false
-      }
-    };
-    return config;
-  }
-};
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	framework: '@storybook/react',
+	core: {
+		builder: 'webpack5',
+	},
+}
